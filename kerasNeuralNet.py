363eca205384b0ac510e0507c62c17585a1ddba9
ark9719 <ark9719@rit.edu>
2018-12-02 13:43:03 -0500

    Fix Keras to RMSE, ignore csv

--- kerasNeuralNet.py
+++ kerasNeuralNet.py
@@ -1,7 +1,8 @@
 import numpy
-import pandas
+import pandas as pd
 from keras.models import Sequential
 from keras.layers import Dense
+from keras import backend as k
 from keras.wrappers.scikit_learn import KerasRegressor
 from keras.callbacks import EarlyStopping
 from sklearn.model_selection import cross_val_score
@@ -11,7 +12,7 @@ from sklearn.pipeline import Pipeline
 from math import sqrt
 
 # load dataset
-train_data = pandas.read_csv("train_processed.csv")
+train_data = pd.read_csv("train_processed.csv")
 train_X = train_data.drop(columns=['fare_amount'])
 train_y = train_data[['fare_amount']]
 
@@ -27,8 +28,10 @@ model.add(Dense(512, activation='relu'))
 model.add(Dense(256, activation='relu'))
 model.add(Dense(1))
 
+def root_means_squared_error(y_true, y_pred):
+    return k.sqrt(k.mean(k.square(y_pred - y_true)))
 #compile model using mse as a measure of model performance
-model.compile(optimizer='adam', loss='mean_squared_error')
+model.compile(optimizer='adam', loss=root_means_squared_error)
 
 #set early stopping monitor so the model stops training when it won't improve anymore
 early_stopping_monitor = EarlyStopping(patience=3)
@@ -36,3 +39,15 @@ early_stopping_monitor = EarlyStopping(patience=3)
 model.fit(train_X, train_y, validation_split=0.2, epochs=30, callbacks=[early_stopping_monitor])
 
 
+
+#utility function for kaggle submission
+def make_submission(model):
+    test_data_x = pd.read_csv("test_processed.csv")
+    submission = (pd.read_csv("sample_submission.csv"))
+    submission = submission.drop(columns=['fare_amount'])
+    test_y_predictions = model.predict(test_data_x)
+    test_y_predictions_df = pd.DataFrame(test_y_predictions)
+    print(test_y_predictions_df.head())
+    submission = submission.assign(fare_amount=test_y_predictions_df)
+    #submission = pd.concat([keys, test_y_predictions_df], ignore_index=True)
+    submission.to_csv("submission.csv", index=False)
