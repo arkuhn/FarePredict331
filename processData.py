d23c7ff4e6bb319d12bbd0f160c16ed26fe5a50b
Wade Mauger <wadeanthony0100@gmail.com>
2018-12-03 16:01:29 -0500

    Add isHoliday and normalized passenger count features

--- processData.py
+++ processData.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 import geopy.distance
 import pandas as pd
-import numpy as np 
+import numpy as np
+import holidays
 
 #NYC Bounding box
 LONGITUDE_MIN = -74.263242
@@ -9,6 +10,8 @@ LONGITUDE_MAX = -72.986532
 LATITUDE_MIN = 40.568973
 LATITUDE_MAX = 41.709555
 
+MIN_PASSENGERS = 1
+MAX_PASSENGERS = 7
 
 def cleanData(features):
     cleanedFeatures = features[
@@ -41,10 +44,17 @@ def isWeekend(dt):
         return 0
     return 1
 
+def isHoliday(dt):
+    #[(month, day), (month, day)...]
+    us_holidays = holidays.UnitedStates()
+    if datetime(dt.year, dt.month, dt.day) in us_holidays:
+        return 1
+    return 0
+
 def processDates(datetimeString):
     dtObject = datetime.strptime(datetimeString, '%Y-%m-%d %H:%M:%S %Z')
 
-    return (isRushour(dtObject), isWeekend(dtObject))
+    return (isRushour(dtObject), isWeekend(dtObject), isHoliday(dtObject))
 
 def calculateDistance(lon1, lat1, lon2, lat2):
     coords1 = (lat1, lon1)
@@ -54,6 +64,9 @@ def calculateDistance(lon1, lat1, lon2, lat2):
     distance = geopy.distance.vincenty(coords1, coords2).km
 
     return distance
+
+def normalizePassengerCount(count):
+    return count / MAX_PASSENGERS
 
 def processData(row):
     lon1 = row['pickup_longitude']
@@ -65,12 +78,14 @@ def processData(row):
     dt = row['pickup_datetime']
 
     #Extract date time features
-    isRushhour, isWeekend = processDates(dt)
+    isRushhour, isWeekend, isHoliday = processDates(dt)
 
     #Consolidate lat/lons into distance
     distance = calculateDistance(lon1, lat1, lon2, lat2)
 
-    return (isRushhour, isWeekend, distance)
+    normalizedPC = normalizePassengerCount(row['passenger_count'])
+
+    return (isRushhour, isWeekend, isHoliday, normalizedPC, distance)
 
 def main():
     count = 0
@@ -82,10 +97,10 @@ def main():
         featureFrame = cleanData(chunk)
 
         #Engineer features
-        featureFrame['isRushHour'], featureFrame['isWeekend'], featureFrame['distance'] = zip(*featureFrame.apply(processData, axis=1))
+        featureFrame['isRushHour'], featureFrame['isWeekend'], featureFrame['isHoliday'], featureFrame['normalizedPC'], featureFrame['distance'] = zip(*featureFrame.apply(processData, axis=1))
 
         #Drop leftover raw data
-        featureFrame.drop(['key', 'pickup_latitude', 'pickup_longitude', 'dropoff_latitude', 'dropoff_longitude', 'pickup_datetime'], axis = 1, inplace=True)
+        featureFrame.drop(['key', 'pickup_latitude', 'pickup_longitude', 'dropoff_latitude', 'dropoff_longitude', 'pickup_datetime', 'passenger_count'], axis = 1, inplace=True)
 
         if(count == 0):
             featureFrame.to_csv('train_processed.csv', mode='a', header=True, index=False)
